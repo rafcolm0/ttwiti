27dd9c71691f2f55aea940b61e85794db65e9760
rafcolm0 <rafael.colon5@gmail.com>
2016-09-25 13:26:23 -0400

    adder

--- tweti_0.1/settings.cpp
+++ tweti_0.1/settings.cpp
@@ -1,4 +1,27 @@
 #include <libconfig.h>
-int set(){
-  return 0;
-}
+#include <fstream>
+#include <unistd.h>
+#include <cstdlib>
+using namespace std;
+
+class settings{
+  
+public:
+  settings(){
+    if(chdir("/home/rc/.config/ttwiti/") != 0){
+      system("mkdir /home/rc/.config/ttwiti/");
+      chdir("/home/rc/.config/ttwiti/");
+    }
+    prefs.open ("prefs", ios::out | ios::app | ios::in);    
+  }
+
+  ~settings(){
+    prefs.close();
+  }
+
+  int numberOfAccounts(){
+    return 0;
+  }
+private:
+  ofstream prefs;
+};
--- tweti_0.1/twiti.cpp
+++ tweti_0.1/twiti.cpp
@@ -1,21 +1,3 @@
-#include <gtk/gtk.h>
-#include <libappindicator/app-indicator.h>
-#include <stdlib.h>
-#include <cstdlib>
-#include <cstring>
-#include <iostream>
-#include <libnotify/notify.h>
-#include <fstream>
-
-using namespace std;
-
-GtkWidget* indicator_menu;
-GtkActionGroup* action_group;
-GtkUIManager*   uim;
-AppIndicator* indicator;
-GError* error = NULL;
-ofstream prefs;
-
 /** author and twitter keys owner: rafcolm_
  ** twitter owner ID	1903897902
  ** consumer key:	L1BkxDo0Uqd6x4B6NS2MTfen8
@@ -28,6 +10,26 @@ ofstream prefs;
  ** External libraries used:
  **   -twitcurl (twitter API): https://github.com/swatkat/twitcurl 
  **/
+#include <gtk/gtk.h>
+#include <libappindicator/app-indicator.h>
+#include <stdlib.h>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <libnotify/notify.h>
+#include "settings.cpp"
+
+using namespace std;
+
+GtkWidget* indicator_menu;
+GtkActionGroup* action_group;
+GtkUIManager*   uim;
+AppIndicator* indicator;
+GError* error = NULL;
+ofstream preferences;
+
+
+
 static void activate_action (GtkAction *action);
 
 static GtkActionEntry entries[] = {
@@ -56,7 +58,7 @@ static const gchar *ui_info =
   "</ui>";
 
 void terminate_prog(){
-  prefs.close();
+  preferences.close();
   exit(0);
 }
 
@@ -139,16 +141,50 @@ static void activate_action(GtkAction *action){
   }
 }
 
+
+static void accountAdder(){
+  GtkWidget *window;
+  GtkWidget *table;
+  GtkWidget *label1;
+  GtkWidget *label2;
+  GtkWidget *label3;
+  GtkWidget *entry1;
+  GtkWidget *entry2;
+  GtkWidget *entry3;
+
+  window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+  gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+  gtk_window_set_title(GTK_WINDOW(window), "GtkEntry");
+
+  table = gtk_table_new(3, 2, FALSE);
+  gtk_container_add(GTK_CONTAINER(window), table);
+
+  label1 = gtk_label_new("Name");
+  label2 = gtk_label_new("Age");
+  label3 = gtk_label_new("Occupation");
+
+  gtk_table_attach(GTK_TABLE(table), label1, 0, 1, 0, 1, (GTK_FILL), (GTK_FILL), 5, 5);
+  gtk_table_attach(GTK_TABLE(table), label2, 0, 1, 1, 2, (GTK_FILL), (GTK_FILL), 5, 5);
+  gtk_table_attach(GTK_TABLE(table), label3, 0, 1, 2, 3, (GTK_FILL), (GTK_FILL), 5, 5);
+
+  entry1 = gtk_entry_new();
+  entry2 = gtk_entry_new();
+  entry3 = gtk_entry_new();
+
+  gtk_table_attach(GTK_TABLE(table), entry1, 1, 2, 0, 1, (GTK_FILL), (GTK_FILL), 5, 5);
+  gtk_table_attach(GTK_TABLE(table), entry2, 1, 2, 1, 2, (GTK_FILL), (GTK_FILL), 5, 5);
+  gtk_table_attach(GTK_TABLE(table), entry3, 1, 2, 2, 3, (GTK_FILL), (GTK_FILL), 5, 5);
+
+  gtk_widget_show_all(window);
+  g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+}
+
 int main(int argc, char **argv){
-  if(chdir("/home/rc/.config/ttwiti/") != 0){
-    system("mkdir /home/rc/.config/ttwiti/");
-    chdir("/home/rc/.config/ttwiti/");
-  }
-  prefs.open ("prefs", ios::out | ios::app | ios::in);
-  if(){
-    
-  }
   gtk_init(&argc, &argv);
+  settings preferences;
+  if (preferences.numberOfAccounts() == 0){
+    accountAdder();
+  }
   /* Menus */
   action_group = gtk_action_group_new("AppActions");
   gtk_action_group_add_actions(action_group, entries, n_entries, NULL);
